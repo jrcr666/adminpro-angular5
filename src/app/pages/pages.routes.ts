1c496e017976895b3d5cf13729cd2dd6db7c5a4b
Juanra <jrcr_87@hotmail.com>
2018-01-07 17:53:53 +0100

    Fin de la sección 8

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -5,16 +5,19 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
-
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 const ROUTES: Routes = [
 	{
 		path: '',
 		component: PagesComponent,
 		children: [
-			{ path: 'dashboard', component: DashboardComponent },
-			{ path: 'account-settings', component: AccountSettingsComponent },
-			{ path: 'progress', component: ProgressComponent },
-			{ path: 'graficas1', component: Graficas1Component },
+			{ path: 'dashboard', component: DashboardComponent, data: { titulo: 'Dashboard' } },
+			{ path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Settings' } },
+			{ path: 'progress', component: ProgressComponent, data: { titulo: 'ProgressBars' } },
+			{ path: 'graficas1', component: Graficas1Component, data: { titulo: 'Gráficas' } },
+			{ path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' } },
+			{ path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJs' } },
 			{ path: '', pathMatch: 'full', redirectTo: '/dashboard' },
 		]
 	},
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-breadcrumbs',
-  templateUrl: './breadcrumbs.component.html',
-  styles: []
+	selector: 'app-breadcrumbs',
+	templateUrl: './breadcrumbs.component.html',
+	styles: []
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
+	public label: string = '';
 
-  ngOnInit() {
-  }
+	constructor(
+		private _router: Router,
+		public title: Title,
+		public meta: Meta) {
+
+		this.getdataRoute().subscribe(data => {
+			console.log(data);
+			this.label = data.titulo;
+			this.title.setTitle(this.label);
+
+			let metaTag: MetaDefinition {
+				name: 'description',
+				content: this.label,
+			}
+
+			this.meta.updateTag(metaTag)
+		})
+	}
+
+	ngOnInit() {
+	}
+
+	getdataRoute() {
+		return this._router.events
+			.filter(evento => evento instanceof ActivationEnd)
+			.filter((evento: ActivationEnd) => evento.snapshot.firstChild === null)
+			.map((evento: ActivationEnd) => evento.snapshot.data)
+	}
 
 }
